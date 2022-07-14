9c34c0fd7cfdea7cc54e521064b1f79232311db2
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-14 14:21:16 -0500

    Created HTML structure of tic tac toe table. Will add styling next t

--- components/TicTacToe.js
+++ components/TicTacToe.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default function TicTacToe() {
+  const Cell = () => <td>-</td>;
+
   return (
     <div>
       <h1>Tic Tac Toe</h1>
+      <table>
+        <tbody>
+          <tr>
+            <Cell />
+            <Cell />
+            <Cell />
+          </tr>
+          <tr>
+            <Cell />
+            <Cell />
+            <Cell />
+          </tr>
+          <tr>
+            <Cell />
+            <Cell />
+            <Cell />
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
