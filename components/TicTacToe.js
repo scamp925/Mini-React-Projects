9191ec8ea7f36d6d7b766313612cb7bd8e717702
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-14 14:59:58 -0500

    Assigned each square a prop of num with a unique number to be assoicated with that square. Wrote a state for turns and a state for filling the squares.

--- components/TicTacToe.js
+++ components/TicTacToe.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
 export default function TicTacToe() {
-  const Square = () => <td>-</td>;
+  const [turn, setTurn] = useState('X');
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  const handleClick = (num) => {
+    const boxes = [...squares];
+
+    if (turn === 'X') {
+      boxes[num] = 'X';
+      setTurn('O');
+    } else {
+      boxes[num] = 'O';
+      setTurn('X');
+    }
+
+    setSquares(boxes);
+  };
+
+  // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events
+  const Square = ({ num }) => <td onClick={() => handleClick(num)}>{squares[num]}</td>;
+
+  Square.propTypes = {
+    num: propTypes.integer,
+  };
+
+  Square.defaultProps = {
+    num: 0,
+  };
 
   return (
     <div>
       <h1>Tic Tac Toe</h1>
       <table>
+        Whose Turn: {turn}
         <tbody>
           <tr>
-            <Square />
-            <Square />
-            <Square />
+            <Square num={0} />
+            <Square num={1} />
+            <Square num={2} />
           </tr>
           <tr>
-            <Square />
-            <Square />
-            <Square />
+            <Square num={3} />
+            <Square num={4} />
+            <Square num={5} />
           </tr>
           <tr>
-            <Square />
-            <Square />
-            <Square />
+            <Square num={6} />
+            <Square num={7} />
+            <Square num={8} />
           </tr>
         </tbody>
       </table>
